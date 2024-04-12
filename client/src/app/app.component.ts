01bc3e8493914f2570908324e7584a8f517f6772
RiteshPhadtare2004 <riteshphadtare12.02.2004@gmail.com>
2024-04-13 00:43:27 +0530

    nav bar updated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginSignupComponent } from './components/login-signup/login-signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,LoginSignupComponent],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
