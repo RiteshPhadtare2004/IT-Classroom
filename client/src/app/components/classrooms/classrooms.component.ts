84ee10b0dc533ea0159f3f367044bd08b10608c2
RiteshPhadtare2004 <riteshphadtare12.02.2004@gmail.com>
2024-04-13 22:55:11 +0530

    Added classroom component and footer component

---
+++ client/src/app/components/classrooms/classrooms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classrooms',
+  standalone: true,
+  imports: [],
+  templateUrl: './classrooms.component.html',
+  styleUrl: './classrooms.component.css'
+})
+export class ClassroomsComponent {
+
+}
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ClassroomsComponent } from '../classrooms/classrooms.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ClassroomsComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
