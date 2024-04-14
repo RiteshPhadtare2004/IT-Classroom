942bcc21fef3a20d54eab54494f7826f94664886
RiteshPhadtare2004 <riteshphadtare12.02.2004@gmail.com>
2024-04-14 12:43:26 +0530

    Navigation is working properly

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginSignupComponent } from './components/login-signup/login-signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { ClassroomsComponent } from './components/classrooms/classrooms.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NoticesComponent } from './components/notices/notices.component';
+import { LoginSignupComponent } from './components/login-signup/login-signup.component';
 
 export const routes: Routes = [
   {
@@ -9,11 +12,17 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'login',
-    component: LoginSignupComponent,
+    path:'login',
+    component: LoginSignupComponent
   },
   {
     path: 'home',
     component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'classroom', pathMatch: 'full' },
+      { path: 'classroom', component: ClassroomsComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'notices', component: NoticesComponent },
+    ]
   },
 ];
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ClassroomsComponent } from '../classrooms/classrooms.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, ClassroomsComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent, ClassroomsComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
