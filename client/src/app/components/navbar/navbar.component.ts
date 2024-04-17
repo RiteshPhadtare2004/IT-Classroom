d396478ab27e65ff18db55793d7120139b3d0ba2
Ritesh Phadtare <138662944+RiteshPhadtare2004@users.noreply.github.com>
2024-04-17 10:10:40 +0530

    dynamic nacbar implemented

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CommonModule, FormsModule, HttpClientModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+    routes:any='';
+    type:string=''
+    studentRoutes : any = {
+      'home':'/home',
+      'profile':'/home/profile',
+      'notices': '/home/notices'
+    };
+    
+    teacherRoutes : any={
+      'home':'/teacher',
+      'profile':'#',
+      'notices': '/home/notices',
+      'createClassroom': '/teacher/createClassroom'
+    }
+    ngOnInit(): void {
+        const userDataString = localStorage.getItem('userData');
+        if (userDataString !== null) {
+          // Parse the JSON string back to an object
+          const userData = JSON.parse(userDataString);
+          if(userData.role == 'student'){
+            this.type = 'student'
+            this.routes = this.studentRoutes
+          }else{
+            this.type = 'teacher'
+            this.routes = this.teacherRoutes
+          }
+        } else {
+          console.log('No userData found in local storage');
+        }
 
+    }
 }
--- client/src/app/components/teacher-layout/teacher-layout.component.ts
+++ client/src/app/components/teacher-layout/teacher-layout.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-teacher-layout',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent,NavbarComponent,CommonModule, FormsModule, HttpClientModule],
   templateUrl: './teacher-layout.component.html',
   styleUrl: './teacher-layout.component.css'
 })
